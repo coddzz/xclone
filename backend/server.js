54ddeb9fbd0baf6c5823d1101c146b34b59117ae
Sarath <learn.cypher@gmail.com>
2025-07-26 02:33:37 +0530

    backend server setup

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const express = require("express")
 
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("hello world!");
-})
+app.get("/", (req, res)=>{
+    res.send("Hello World!")
+    console.log("Web Page Reloded!")
+});
 
 app.listen(3000, () =>{
     console.log("server running on port 3000")
