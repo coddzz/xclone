2fc98d860dc0407e2941be65b668c149e39274ba
Sarath <learn.cypher@gmail.com>
2025-08-02 02:56:04 +0530

    Creating User Data!

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,31 +1,65 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) =>{
-    try {
-        const {username, fullname, email, password} = req.body;
+    try{
+        const {username, fullname, email, password } = req.body;
+        //app.use(express.json());
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //email validation
         if(!emailRegex.test(email)){
-            return res.status(400).json({error:"Invalid email format"})
+            return res.status(400).json({error:"Invalid email format!"})
         }
 
-        const existingUser = await User.findOne({email})// or check with username also
-        const existingUsername = await User.findOne({username})
-        if ( existingUser || existingUsername){
-            return res.status(400).json({error: "Already Existing Username or Email"})
+        // checking if email and username already exists
+        const existingEmail= await User.findOne({email : email});
+        if(existingEmail){
+            return res.status(400).json({error:"Email Already Exists!"})
+        }
+        // Cheking Username
+        const existingUsername = User.findOne({username : username});
+        if(existingUsername){
+            return res.status(400).json({error:"Username Already Exists!"})
         }
 
-    } catch (error){
-        console.log(`Error in signup controller: ${error}`)
-        res.status(500).json({error:"Internal Server Error"})
+        //cheking password atleast 6
+        if(!password.length <= 6 ){
+            return res.status(400).json({error:"Password Must Have 6 Letters!"});
+        }
+
+        // password hashing using bcryptjs
+        const salt = await bcript.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        // creating new user data
+        const newUser = new User({
+            username : username,
+            fullname : fullname,
+            email : email,
+            password : hashedPassword
+
+        })
+
+        if (newUser){
+            await newUser.save();
+            res.status(200).json({Status: "User Created Successfully!"})
+            
+        }
+        else{
+            res.status(400).json({error: "Invalid User Data!"});
+        }
+
+    } catch (error) {
+       console.log(`Error in signup controller ${error}`);
+       res.status(500).json({error: "Internal Server Error!"});
     }
-    
 }
 
 export const login = (req, res) =>{
-    res.send("Login Page..")
+    res.send("login..")
 }
 
 export const logout = (req, res) =>{
-    res.send("logout")
+    res.send("logout..")
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,8 +3,8 @@ import {signup,login,logout} from "../controllers/auth.controller.js"
 
 const route = express.Router();
 
-route.post("/signup", signup)
-route.post("/login", login)
-route.post("/logout", logout)
+route.get("/signup", signup)
+route.get("/login", login)
+route.get("/logout", logout)
 
 export default route;
