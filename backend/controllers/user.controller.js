38aadff7ad36c02142005375a530e308c37b39fa
Sarath <learn.cypher@gmail.com>
2025-08-23 21:31:13 +0530

    followUnfollowUser

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -19,3 +19,43 @@ export const getUserProfile = async (req, res) =>{
         res.status(500).json({error: error.message});
     }
 }
+
+export const followUnfollowUser = async (req, res) =>{
+
+    try{
+        const {id} = req.params;
+        const userToModify = await User.findById.(id)
+        const currentUser = await User.findById.(req.user._id)
+
+        if (id === req.user_id.toString()){
+            return res.status(400).json({error:"You can't follow/Unfollow your self!"})
+        }
+
+        if (!userToModify || !currentUser){
+            return res.status(404).json(error:"User Not Found!")
+        }
+
+        const iSFollowing = currentUser.following.Includes(id);
+
+        if(iSFollowing){
+            //unfollow
+            await User.findByIdAndUpdate(id, { $pull: { followers : req.user._id }})
+            await User.findByIdAndUpdate(req.user._id, { $pull: { following : id }})
+
+            return res.status(200).json({message :"Unfollow Successfull!"})
+        } else{
+            //follow
+            await User.findByIdAndUpdate(id, { $push: { followers : req.user._id}})
+            await User.findByIdAndUpdate(req.user._id, { $push: { followers : id}})
+
+            //set notificaton...
+
+            return res.status(200).json({message:"User Followed Successfully!"})
+        }
+
+    } catch(err){
+        console.log(`Error in followUnfollowUser controller: ${err.message}`)
+        res.status(500).json({error: err.message})
+    }
+}
+
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
     
         username : {
         type: String,
@@ -54,6 +54,6 @@ const UserSchema = mongoose.Schema({
 
 }, {timestamps : true } )
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
