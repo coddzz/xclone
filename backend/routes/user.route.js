79e300f45ba4c3747f40b7d29e8108827ced6361
Sarath <learn.cypher@gmail.com>
2025-08-23 22:05:30 +0530

    getSuggestedUsers

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUserProfile, followUnfollowUser } from "../controllers/user.controller.js";
+import { getUserProfile, followUnfollowUser, getSuggestedUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 
 
 
