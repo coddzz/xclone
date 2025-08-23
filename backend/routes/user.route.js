27bbc589688e374826cb63a12dbb5a60211939f0
Sarath <learn.cypher@gmail.com>
2025-08-23 22:28:57 +0530

    user.controller.js/updateUser

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUserProfile, followUnfollowUser, getSuggestedUsers } from "../controllers/user.controller.js";
+import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 router.get("/suggested", protectRoute, getSuggestedUsers);
-
+router.get("/updateuser", protectRoute, updateUser);
 
 
 export default router;
