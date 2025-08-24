7c7c3adcfa49c14681e6c62b109b7372e0d51096
Sarath <learn.cypher@gmail.com>
2025-08-25 00:50:37 +0530

    post.controller.js  deletePost

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, deletePost} from '../controllers/post.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.post('/create', protectRoute, createPost)
+router.post('/create', protectRoute, createPost);
+router.post('/delete', protectRoute, deletePost);
 
 
 
