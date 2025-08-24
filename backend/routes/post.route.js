1ab01c82c326d4c1b3fcb1e5b0580a18d0d66abd
Sarath <learn.cypher@gmail.com>
2025-08-24 23:40:47 +0530

    post.controller.js  createPost

---
+++ backend/routes/post.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createPost } from '../controllers/post.controller.js';
+import protectRoute from '../middleware/protectRoute.js';
+
+const router = express.Router();
+
+router.post('/create', protectRoute, createPost)
+
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 //const express = require("express")
-import express from "express"
-import dotenv from "dotenv"
-import cloudinary from "cloudinary"
-import connectDB from "./db/connectDB.js"
-import cookieParser from "cookie-parser"
-import authRoute from "./routes/auth.route.js"
-import userRoute from "./routes/user.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
 
 const app = express();
 dotenv.config();
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 
 
