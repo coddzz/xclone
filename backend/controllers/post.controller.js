2879beef5e14d6b91b7840d58ecb557b045a0843
Sarath <learn.cypher@gmail.com>
2025-08-25 19:48:39 +0530

    commentOnPost  in post.controller.js

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -59,3 +59,33 @@ export const deletePost = async (req, res) => {
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
+
+export const commentOnPost = async (req, res) =>{
+
+	try{
+		const { text } = req.body;
+		const postId = req.params.id;
+		const userId = req.user._id;
+
+		if (!text) {
+			return res.status(400).json({ error: "Text field is required" });
+		}
+		const post = await Post.findById(postId);
+
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		const comment = { user: userId, text };
+
+		post.comments.push(comment);
+		await post.save();
+
+		req.status(200).json(post);
+
+	} catch(err){
+		console.log(`Error in commentOnPost in postcontroller: ${err.message}`)
+		res.status(500).json({error: err.message});
+	}
+
+};
