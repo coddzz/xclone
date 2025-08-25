521c6579829742ab29df708485689e438dac3b25
Sarath <learn.cypher@gmail.com>
2025-08-25 22:31:58 +0530

    likeUnlinePost  in post.controller.js

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -88,4 +89,47 @@ export const commentOnPost = async (req, res) =>{
 		res.status(500).json({error: err.message});
 	}
 
+};
+
+export const likeUnlikePost = async (req, res) => {
+	try {
+		
+		const { id: postId } = req.params;
+		const userId = req.user._id;
+
+		const post = await Post.findById(postId);
+
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		const userLikedPost = post.likes.includes(userId);
+
+		if (userLikedPost) {
+			// Unlike post
+			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+			await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
+
+			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+			res.status(200).json(updatedLikes);
+		} else {
+			// Like post
+			post.likes.push(userId);
+			await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+			await post.save();
+
+			const notification = new Notification({
+				from: userId,
+				to: post.user,
+				type: "like",
+			});
+			await notification.save();
+
+			const updatedLikes = post.likes;
+			res.status(200).json(updatedLikes);
+		}
+	} catch (error) {
+		console.log("Error in likeUnlikePost controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
 };
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
+router.delete("/likes/:id", protectRoute, likeUnlikePost);
 
 export default router;
