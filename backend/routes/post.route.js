4b2298e0e6600ee22eeebc65218f9950b315c66f
Sarath <learn.cypher@gmail.com>
2025-08-26 01:15:37 +0530

    getAllPosts  in post.controller.js

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, getAllPosts, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
-router.delete("/likes/:id", protectRoute, likeUnlikePost);
+router.post("/likes/:id", protectRoute, likeUnlikePost);
+router.get("/all", protectRoute, getAllPosts);
 
 export default router;
