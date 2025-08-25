c803bb4632714ea348d533169d8aff8d343934c4
Sarath <learn.cypher@gmail.com>
2025-08-26 02:25:50 +0530

    getFollowingPosts, getUserPosts

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
 import { 
+    commentOnPost,
     createPost,
     deletePost, 
     getAllPosts, 
+    getFollowingPosts, 
     getLikedPosts, 
+    getUserPosts, 
     likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
@@ -12,7 +15,11 @@ const router = express.Router();
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+
 router.get("/all", protectRoute, getAllPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 
 export default router;
