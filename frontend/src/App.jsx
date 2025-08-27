11f2f07463da94472b7e036a8555da18f0023a50
Sarath <learn.cypher@gmail.com>
2025-08-27 11:37:05 +0530

    frontend - React+Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignupPage";
+import LoginPage from "./pages/auth/signup/LoginPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notifications/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+
+const App = () => {
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar/> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel/>
+    </div>
+
+  );
+};
+
+export default App;
