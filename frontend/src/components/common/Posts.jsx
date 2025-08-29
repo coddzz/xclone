9a096675d96c081f432c7f95413c089dc6bc9a91
Sarath <learn.cypher@gmail.com>
2025-08-29 21:27:58 +0530

    forYou/following route testing

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,9 +1,27 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { POSTS } from "../../utils/db/dummy";
+import {baseUrl} from "../../constants/url.js";
 
-const Posts = () => {
+const Posts = ({feedType}) => {
 	const isLoading = false;
+
+	const getPostEndPoint = ()=>{
+		switch(feedType){
+			
+			case "forYou" : 
+			return `${baseUrl}/api/posts/all`;
+			
+			case "following":
+				return `${baseUrl}/api/posts/following`;
+
+			default :
+				return `${baseUrl}/api/posts/all`;
+		}
+	}
+
+	const POST_ENDPOINT = getPostEndPoint()
+	console.log(POST_ENDPOINT);
 
 	return (
 		<>
