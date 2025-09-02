30b4458d9101930297fa565d674945c66bd29e1f
Sarath <learn.cypher@gmail.com>
2025-09-02 21:19:33 +0530

    Edit user's profile __ EditProfileModal

--- frontend/src/pages/profile/EditProfileModal.jsx
+++ frontend/src/pages/profile/EditProfileModal.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { baseUrl } from "../../constants/url";
+import toast from "react-hot-toast";
+import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 const EditProfileModal = () => {
 	const [formData, setFormData] = useState({
-		fullName: "",
+		fullname: "",
 		username: "",
 		email: "",
 		bio: "",
@@ -11,9 +15,68 @@ const EditProfileModal = () => {
 		currentPassword: "",
 	});
 
+	const queryClient = useQueryClient();
+	const {data:authUser} = useQuery({ queryKey: ["authUser"]});
+	const { mutate: updateProfile, isPending : isUpdatingProfile} = useMutation({
+		mutationFn: async ( {fullname, username, email, bio,
+				link, newPassword, currentPassword} )=>{
+			try{
+				const res = await fetch(`${baseUrl}/api/users/update`,{
+					method:"POST",
+					credentials:"include",
+					headers:{
+						"Content-Type":"application/json"
+					},
+					body : JSON.stringify({
+						fullname,
+						username,
+						email,
+						bio,
+						link,
+						newPassword,
+						currentPassword
+					})
+				})
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong!")
+				}
+				return data;
+
+			} catch(error){
+				throw error;
+			}
+		},
+		onSuccess: ()=>{
+			toast.success("Profile Update Successfully!")
+			Promise.all([
+				queryClient.invalidateQueries({queryKey:["authUser"]}),
+				queryClient.invalidateQueries({queryKey:["userProfile"]}),
+			])
+		},
+		onError : (error) =>{
+			toast.error(error.message);
+		}
+	})
+
+
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
+
+	useEffect(()=>{  // fill old values in coloms. its avoid empty data
+		if (authUser){
+			setFormData({
+				fullname : authUser.fullname,
+				username : authUser.username,
+				email : authUser.email,
+				bio : authUser.bio,
+				link : authUser.link,
+				newPassword : "",
+				currentPassword : "",
+			})
+		}
+	},[authUser])
 
 	return (
 		<>
@@ -30,7 +93,7 @@ const EditProfileModal = () => {
 						className='flex flex-col gap-4'
 						onSubmit={(e) => {
 							e.preventDefault();
-							alert("Profile updated successfully");
+							updateProfile(formData);
 						}}
 					>
 						<div className='flex flex-wrap gap-2'>
@@ -38,8 +101,8 @@ const EditProfileModal = () => {
 								type='text'
 								placeholder='Full Name'
 								className='flex-1 input border border-gray-700 rounded p-2 input-md'
-								value={formData.fullName}
-								name='fullName'
+								value={formData.fullname}
+								name='fullname'
 								onChange={handleInputChange}
 							/>
 							<input
@@ -94,7 +157,10 @@ const EditProfileModal = () => {
 							name='link'
 							onChange={handleInputChange}
 						/>
-						<button className='btn btn-primary rounded-full btn-sm text-white'>Update</button>
+						<button className='btn btn-primary rounded-full btn-sm text-white'>
+							{isUpdatingProfile && <LoadingSpinner size="sm"/>}
+							{!isUpdatingProfile && "Update"}
+						</button>
 					</form>
 				</div>
 				<form method='dialog' className='modal-backdrop'>
